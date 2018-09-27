87d7d852268f6241865c20052e81b0b32c802615
Anne O'Malley <anneo@accretivetg.com>
2018-09-26 22:32:30 -0700

    eslint stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,29 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
 
 gulp.task("default", ["styles"], function() {
   gulp.watch("sass/**/*.scss", ["styles"]);
   browserSync.init({
     server: "./"
   });
+});
+
+gulp.task('lint', function() {
+  return (
+      gulp
+          .src(['js/**/*.js'])
+          // eslint() attaches the lint output to the eslint property
+          // of the file object so it can be used by other modules.
+          .pipe(eslint())
+          // eslint.format() outputs the lint results to the console.
+          // Alternatively use eslint.formatEach() (see Docs).
+          .pipe(eslint.format())
+          // To have the process exit with an error code (1) on
+          // lint error, return the stream and pipe to failOnError last.
+          .pipe(eslint.failOnError())
+  );
 });
 
 gulp.task("styles", function() {
