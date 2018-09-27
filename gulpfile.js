6c2a9ecad5f72ae81be54f9fb8af7401f6e0eb23
Anne O'Malley <anneo@accretivetg.com>
2018-09-26 22:11:20 -0700

    removed lesson folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 
 gulp.task("default", ["styles"], function() {
   gulp.watch("sass/**/*.scss", ["styles"]);
+  browserSync.init({
+    server: "./"
+  });
 });
 
 gulp.task("styles", function() {
